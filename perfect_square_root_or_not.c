3df0d63ec3ae45446e53ce5f887fc5848201f9e5
SAI VARDHAN BATHSALA <119719060+22A91A04I9@users.noreply.github.com>
2023-01-21 21:19:54 +0530

    #include<stdio.h> #include<math.h> int main() {     int m,n;     float p;     scanf("%d",&n);     p=sqrt(n);     m=p;     if(p==m)     {         printf("True");     }     else     {         printf("False");     } }

--- perfect_square_root_or_not.c
+++ perfect_square_root_or_not.c
@@ -1,17 +1,18 @@
-# include <stdio.h>
-# include <math.h> 
-int main() 
-{ 
-	int m, n ; 
-	float p ;
- 	scanf("%d", &n) ; 
- 	p = sqrt(n) ; 
- 	m = p ; 
- 	if (p == m) 
-  		printf("True"); 
- 	else 
-  		printf("False"); 
- 	return 0;
-} 
-			
-			
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int m,n;
+    float p;
+    scanf("%d",&n);
+    p=sqrt(n);
+    m=p;
+    if(p==m)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
